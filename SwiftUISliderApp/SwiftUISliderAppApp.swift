8014087201dfc21e970420c0091ba08b39592642
Igor Gadomskii <igvgad@gmail.com>
2022-05-04 18:40:05 +0300

    Initial Commit

---
+++ SwiftUISliderApp/SwiftUISliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderAppApp.swift
+//  SwiftUISliderApp
+//
+//  Created by Macbook on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
